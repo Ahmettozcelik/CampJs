ce8d14e5696eded9120d77ac8f1b7bcbd14198d5
Ahmettozcelik <ozcelikahmett61@gmail.com>
2022-04-17 11:50:43 +0300

    Test

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ let test = 5;
 test = "Ahmet Levs"
 console.log(test)
 console.log(3);
-let student = {id:1, name="Ahmet"}
+let student = {id:1, name:"Ahmet"}
 console.log(student)
 
 function save() {
