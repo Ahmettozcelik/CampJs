37e32acbc7d73013d9b922cf966d01770f47eef3
Ahmettozcelik <ozcelikahmett61@gmail.com>
2022-04-17 13:06:57 +0300

    Save All

--- src/app.js
+++ src/app.js
@@ -1,21 +1,54 @@
 let test = 5;
 test = "Ahmet Levs"
-console.log(test)
-console.log(3);
+//console.log(test)
+//console.log(3);
 let student = {id:1, name:"Ahmet"}
-console.log(student)
+//console.log(student)
 
 function save(puan=10, ogrenci) {
     console.log(ogrenci.name+":"+puan)
 }
-save(undefined, student);
-
+//save(undefined, student);
 let students= ["Ahmet","Engin","Levs"]
-
-console.log(students)
-
+//console.log(students)
 let students2 = [student, {id:2, name:"Ahmet"}, "İstanbul", {city:"LA"}]
+//console.log(students2)
 
-console.log(students2)
+//rest
+let showProducts = function (id, ...products) {
+    console.log(id)
+    console.log(products)
+}
 
+//console.log(typeof showProducts)
+//showProducts(10, ["Ahmet", "Levs", "Tr", "61"], [["Ahmet", "Levs", "Eu"]])
 
+//spread
+let points = [1,2,5,7,9,6,5,4,3]
+console.log(...points)
+console.log(Math.max(...points))
+console.log(..."Abc","d","efg","h")
+
+//destructure
+let populations = [10000,20000,30000,[40000,100000]]
+let [little,middle,high,[veryHigh,maximum]] = populations
+
+console.log(little)
+console.log(middle)
+console.log(high)
+console.log(veryHigh)
+console.log(maximum)
+
+function someFunc([attr]) {
+    console.log(attr)
+}
+
+someFunc(populations)
+
+let category = {id:1, name:"İçecek"}
+console.log(category.id)
+console.log(category["name"])
+
+let {id, name} = category
+console.log(id)
+console.log(name)
